64a84df3a64d75cc38eb7448a1df1e6a95fe98a2
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-02 15:56:58 -0500

    hoisting

---
+++ Hoisting.js
@@ -0,0 +1,11 @@
+elementos()
+
+function elementos (){
+    return ( "hola" + nombre) 
+}
+
+nombre= "angie"
+
+//se puede llamar la funcion arriba 
+//pero las variables deben colocarse arriba
+//para que se pueda ejecutar la funcion en el return 
