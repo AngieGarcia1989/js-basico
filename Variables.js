918a0fb189e3c96986575417531d8383c9203b8b
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-02 14:23:47 -0500

    Variables

---
+++ Variables.js
@@ -0,0 +1,19 @@
+var nombre = "angie";
+
+//Declarar
+ var edad;
+
+ // inicializar con el = la asignacion 
+ edad = 32;
+
+edad // en esta parte ya esta declarada la variable con el numero asignado
+
+var elementos = ["computador", "celular"]
+
+var persona = {
+    nombre: "angie",
+    edad: 32
+}
+
+
+// De esta manera quedan declaradas las variables asignandoles la informacion correspondiente
