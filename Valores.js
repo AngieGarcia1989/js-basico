a8f590d493b20642c5502e4374a38a9c2f35e3e1
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-02 13:32:12 -0500

    first commit-valores

---
+++ Valores.js
@@ -0,0 +1,19 @@
+// // podemos ir a nuestro navegador y colocar click derecho inspeccionar,
+//  //alli ingresamos a console donde podemos ver el valor de cada valor que le esamos dando 
+//  ya sea un 
+//  numero
+//  String
+//  booleano
+//  objeto
+//  nulo o indefinido
+
+40
+
+"angie" 
+
+true
+false
+
+{nombre:"angie"}
+
+[1,2,3]
