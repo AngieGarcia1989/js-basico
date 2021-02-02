219b08eb9c9347fd406e54fdfa4ad469fdcb3c8e
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-02 17:17:24 -0500

    Valores:True-False

---
+++ Valores:Truthy-Falsy.js
@@ -0,0 +1,17 @@
+false
+Boolean()
+Boolean(NaN);
+Boolean("");
+Boolean(undefined);
+Boolean(0);
+Boolean(null);
+
+
+true 
+Boolean(1);
+Boolean(" ");
+Boolean("a");
+Boolean([]);
+Boolean({});
+Boolean(function(){});
+Boolean(true)
