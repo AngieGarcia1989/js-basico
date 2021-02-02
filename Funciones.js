cb99e25928f06e4eee48864eefc2315a583a39c8
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-02 15:18:11 -0500

    Funciones declativas y expresivas

---
+++ Funciones.js
@@ -0,0 +1,17 @@
+///declarativa
+
+function mifuncion () {
+return 3;
+}
+//llamo la funcion 
+mifuncion()
+
+
+//expresion o anonima
+//paso parametros a,b
+
+var mifuncion = function(a,b) {
+    return a-b;
+}
+//llamo la funcion 
+mifuncion()
