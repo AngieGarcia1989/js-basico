eb21a647ca101b2a1e3974b817f10501c83e2f92
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-04 09:47:26 -0500

    Operadores:Asignacion,Comparacion,Aritmeticos

---
+++ Operadores:Asignacion,Comparacion,Aritmeticos.js
@@ -0,0 +1,19 @@
+//operadores
+
++ , - , * ,/
+
+//comparacion y asignacion
+
+3 == "3"; // compara el valor seria true
+3 === "3"; // compara exactamente el valor y si es string y numero seria false
+
+> , < , <= , >=
+
+a && b // a y b 
+a || b // a o b 
+
+var edad = 40
+edad ++ // aunmenta  a uno en este caso a 41
+
+edad += 2 // aumenta a dos en este caso tomando el 41 
+// seria 43
