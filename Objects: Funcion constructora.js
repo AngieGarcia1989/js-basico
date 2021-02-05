09500499cb10e9da9627724a43d4bed1f4a79075
Angie Garcia <Angiegarciaoficial@gmail.com>
2021-02-05 12:05:56 -0500

    Objetos:funcion constructora

---
+++ Objects: Funcion constructora.js
@@ -0,0 +1,8 @@
+function auto(marca,modelo,annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+var autonuevo = new auto("Tesla", "Model 3", 2020);
+var autonuevo1 = new auto("mazda", "Model 1", 2012);
+var autonuevo2 = new auto("Toyota", "Model 4", 2018);
